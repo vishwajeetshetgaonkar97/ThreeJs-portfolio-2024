977926472634bf8f7a4d555a8159bcc64e2dd67c
vishwajeetshetgaonkar97 <54806391+vishwajeetshetgaonkar97@users.noreply.github.com>
2024-04-02 16:44:08 -0400

    mob nav changes

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,11 +2,16 @@ import Image from 'next/image'
 import styles from './Navigation.module.css'
 import Link from "next/link";
 
+import {  useState } from "react";
+
 const Navigation = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
 
   return (
     
-<div className={styles.navBar}>
+<div className={`${styles.navBar} ${isOpen? styles.navBarActive:""}`}>
 
 
 <Image
@@ -17,12 +22,22 @@ className={styles.logo}
         alt="logo image"
       />
 
-      <div className={styles.linksContainer}>
-        <div> <Link className={styles.link} href="/">Home</Link></div>
-        <div><Link className={styles.link} href="/projects">Projects</Link></div>
-        <div><Link className={styles.link} href="/about">About</Link></div>
-        <div><Link className={styles.link} href="/contact">Contact</Link></div>
+      <div className={styles.linksBurgerContainer} onClick={()=>setIsOpen(!isOpen)}>
+      <Image
+className={`${isOpen? styles.closeIcon: styles.burgerIcon}`}
+        src={isOpen? "/close.png": "/menu.png"}
+        width={100}
+        height={100}
+        alt="logo image"
+      />
 
+      </div>
+
+      <div className={`${styles.linksContainer}  ${isOpen? styles.linksContainerActive : ""}`}>
+        <div> <Link className={styles.link} href="/" onClick={()=>setIsOpen(false)}>Home</Link></div>
+        <div><Link className={styles.link} href="/projects" onClick={()=>setIsOpen(false)}>Projects</Link></div>
+        <div><Link className={styles.link} href="/about" onClick={()=>setIsOpen(false)}>About</Link></div>
+        <div><Link className={styles.link} href="/contact" onClick={()=>setIsOpen(false)}>Contact</Link></div>
       </div>
       </div>
 
