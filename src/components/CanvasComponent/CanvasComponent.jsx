d6af5a66f764faa09a23e023ecd5ac266124e442
vishwajeetshetgaonkar97 <54806391+vishwajeetshetgaonkar97@users.noreply.github.com>
2024-07-06 16:25:14 -0400

    compass addition

--- src/components/CanvasComponent/CanvasComponent.jsx
+++ src/components/CanvasComponent/CanvasComponent.jsx
@@ -2,24 +2,26 @@ import React from "react";
 import { Suspense } from "react";
 import GlobalLoader from "../GlobalLoader/GlobalLoader";
 import { Canvas, useThree } from "@react-three/fiber";
-import {  useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Sky } from "../../models/Sky";
 import { Ship } from "../../models/Ship";
 import { Bird } from "../../models/Bird";
-import styles from './styles.module.css'
+import styles from "./styles.module.css";
+import Image from "next/image";
+import Lottie from "lottie-react";
+import compassAnimation from "../../assets/compass.json";
 
 const CanvasComponent = ({
   isRotating = false,
   setIsRotating,
-  setCurrentStage ,
+  setCurrentStage,
   islandPosition,
-  islandScale ,
-  isOnlyBg=false,
+  islandScale,
+  isOnlyBg = false,
 }) => {
   const [isPlayingMusic, setIsPlayingMusic] = useState(false);
 
   const audioRef = useRef(null);
-
 
   useEffect(() => {
     if (audioRef.current) {
@@ -50,31 +52,76 @@ const CanvasComponent = ({
           <Sky isRotating={isRotating} />
           {/* <NightSky isRotating={isRotating} /> */}
 
-     { !isOnlyBg &&    <Ship
-            isRotating={isRotating}
-            setIsRotating={setIsRotating}
-            setCurrentStage={setCurrentStage}
-            position={islandPosition}
-            scale={islandScale}
-          />}
+          {!isOnlyBg && (
+            <Ship
+              isRotating={isRotating}
+              setIsRotating={setIsRotating}
+              setCurrentStage={setCurrentStage}
+              position={islandPosition}
+              scale={islandScale}
+            />
+          )}
 
           <Bird />
 
           {/* <Island /> */}
         </Canvas>
       </Suspense>
-      <audio ref={audioRef} src="/sakura.mp3" autoPlay loop>
-            Your browser does not support the
-            <code>audio</code> element.
-          </audio>
+      <audio ref={audioRef} src="/sakura.mp3"  loop>
+        Your browser does not support the
+        <code>audio</code> element.
+      </audio>
 
-          <img
-          className={styles.audioIcon}
-            src={!isPlayingMusic ? '/soundoff.png' : `/soundon.png`}
-            alt='jukebox'
-            onClick={() => setIsPlayingMusic(!isPlayingMusic)}
-          />
+      <Image
+        height={50}
+        width={50}
+        className={styles.audioIcon}
+        src={!isPlayingMusic ? "/soundoff.png" : `/soundon.png`}
+        alt="jukebox"
+        onClick={() => setIsPlayingMusic(!isPlayingMusic)}
+      />
 
+  { !isOnlyBg &&   <div className={styles.compassContainer}>
+        <div
+          className={`${styles.compassText} ${styles.compassInnerTText}`}
+          onClick={() => {
+            location.href = "/";
+          }}
+        >
+          Home
+        </div>
+        <div className={styles.compassInnerContainer}>
+          <div
+            className={`${styles.compassText} ${styles.compassInnerText}`}
+            onClick={() => {
+              location.href = "/projects";
+            }}
+          >
+            Project
+          </div>
+          <div className={styles.compassGif}>
+            <Lottie animationData={compassAnimation} loop={true} />
+          </div>
+
+          <div
+            className={`${styles.compassText} ${styles.compassInnerText}`}
+            onClick={() => {
+              location.href = "/about";
+            }}
+          >
+            About
+          </div>
+        </div>
+        <div
+          className={`${styles.compassText} ${styles.compassInnerDText}`}
+          onClick={() => {
+            location.href = "/contact";
+          }}
+        >
+          Contact
+        </div>
+        <div className={styles.bottomInfo}> Press <span> N</span> <span>S</span> <span>E</span> <span>W </span> key to navigate</div>
+      </div>}
     </>
   );
 };
